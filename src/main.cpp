9be5cdae5d53d866fcb9d583c6023305e783e828
Eugen-Lenovo <eugen.haidarli@codwer.com>
2023-11-26 13:40:09 +0200

    implement logic

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include "dd_photoresistor/dd_photoresistor.h"
 
 #define SYS_TICK 1000
+int light;
 
 void setup() {
   setup_RGB();
@@ -12,9 +13,19 @@ void setup() {
 
 void loop() { 
 
-  // Logic to be implemented
+  light = get_photoresistor_value();
+  
+  if(light < 2048){
+    Serial.print("Theres enough light: ");
+    Serial.println(light);
+    turn_off_RGB();
+  }else{
+    Serial.print("Not enough light: ");
+    Serial.println(light);
+    set_RGB_green();
+  }
 
-  turn_off_RGB();
+  // turn_off_RGB();
 
   delay(SYS_TICK);
 }
